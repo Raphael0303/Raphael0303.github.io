54e958eff1e6b52d4d3f87d223c2f0168e760a6a
Raphael <raphael.comps@etu.emse.fr>
2022-09-04 11:33:52 +0200

    Changement Nom

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -12,21 +12,15 @@ const SideBar = () => (
         <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael.l.dangelo@gmail.com">michael.l.dangelo@gmail.com</a></p>
+        <h2>Raphaël Comps</h2>
+        <p><a href="mailto:Raphael.Comps@protonmail.com">Raphael.Comps@protonmail.com</a></p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I like building things.
-        I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni, and
-        the co-founder and CTO of <a href="https://arthena.com">Arthena</a>. Before Arthena I was
-        at <a href="https://matroid.com">Matroid</a>
-        , <a href="https://planet.com">Planet</a>
-        , <a href="https://planetaryresources.com">Planetary Resources</a>
-        , <a href="https://facebook.com">Facebook</a>
-        , and <a href="https://seds.org">SEDS</a>.
+      <p>Hi, I&apos;m Raphaël. I like electronics and silicon.
+        I am a <a href="https://www.mines-stetienne.fr/en/academic/ismin-masters-degree-in-microelectronics-and-computer-science/">Ecole des Mines de Saint Etienne ISMIN</a> Student
       </p>
       <ul className="actions">
         <li>
