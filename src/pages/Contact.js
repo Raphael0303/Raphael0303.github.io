c1ad3a447b56508ad2689fd2813e7447c22773f2
Raphael <raphael.comps@etu.emse.fr>
2022-09-11 21:06:08 +0200

    API

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,7 +8,7 @@ import ContactIcons from '../components/Contact/ContactIcons';
 const Contact = () => (
   <Main
     title="Contact"
-    description="Contact Michael D'Angelo via email @ michael.l.dangelo@gmail.com"
+    description="Contact Raphaël Comps via email @ Raphael.Comps@protonmail.com"
   >
     <article className="post" id="contact">
       <header>
